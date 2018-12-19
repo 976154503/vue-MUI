d8c4d9f0ae21843fcea5e0c138935aa5884b7ad0
zhaojianping <zhaojianping@xinshiyun.com>
2018-12-19 09:26:43 +0800

    init my project

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,27 @@
 <template>
-    <div>
+    <div class="app-container">
         <header id="header" class="mui-bar mui-bar-nav">
 			<a class="mui-action-back mui-icon mui-icon-left-nav mui-pull-left"></a>
 			<h1 class="mui-title">导航栏</h1>
 		</header>
+        <nav class="mui-bar mui-bar-tab">
+			<a class="mui-tab-item mui-active" href="#tabbar">
+				<span class="mui-icon mui-icon-home"></span>
+				<span class="mui-tab-label">首页</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-chat">
+				<span class="mui-icon mui-icon-email"><span class="mui-badge">9</span></span>
+				<span class="mui-tab-label">消息</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-contact">
+				<span class="mui-icon mui-icon-contact"></span>
+				<span class="mui-tab-label">通讯录</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-map">
+				<span class="mui-icon mui-icon-gear"></span>
+				<span class="mui-tab-label">设置</span>
+			</a>
+		</nav>
     </div>
     
 </template>
@@ -12,3 +30,19 @@ export default {
     
 }
 </script>
+<style scoped>
+.app-container {
+    padding: 40px 0 0 0;
+}
+
+.mui-bar {
+    background-color: #583c00;
+}
+
+.mui-title ,.mui-bar .mui-icon {
+    color: floralwhite;
+}
+
+
+</style>
+
--- src/main.js
+++ src/main.js
@@ -4,12 +4,12 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 //http://dev.dcloud.net.cn/mui/ui/
-import './lib/mui/css/mui.css'
+ import './lib/mui/css/mui.css'
+
 
 import app from './App.vue'
 import router from './router.js'
 
-console.log('ok')
 
 var vm = new Vue({
 	el: '#app',
